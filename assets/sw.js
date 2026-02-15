a5d6f4479bd7fdba26b61906887a0d8af973071d
WeiNyn <nguyenhuy17798@gmail.com>
2026-02-15 11:58:23 +0700

    chore: handle cache curroectly

--- assets/sw.js
+++ assets/sw.js
@@ -1,4 +1,5 @@
-var cacheName = 'personars-v0.1.0';
+var APP_VERSION = 'v0.1.0';
+var cacheName = 'personars-' + APP_VERSION;
 var filesToCache = [
   './',
   './index.html',
@@ -6,8 +7,9 @@ var filesToCache = [
   './personars_bg.wasm',
 ];
 
-/* Start the service worker and cache all of the app's content */
+/* Install: cache all files under the new version key */
 self.addEventListener('install', function (e) {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
@@ -15,7 +17,24 @@ self.addEventListener('install', function (e) {
   );
 });
 
-/* Serve cached content when offline */
+/* Activate: purge any caches from older versions */
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== cacheName) {
+            console.log('[SW] Removing old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+/* Cache-first: instant loads, cache invalidation handled by version bump */
 self.addEventListener('fetch', function (e) {
   e.respondWith(
     caches.match(e.request).then(function (response) {
