b7322a7b9aea3467f8ca8e960a783b3c85e1af65
WeiNyn <44496251+WeiNyn@users.noreply.github.com>
2026-02-20 05:46:12 +0000

    Deploying to gh-pages from @ WeiNyn/personars@591eebfd9edaee0546722995b7f7a67450415b4d 🚀

---
+++ sw.js
@@ -0,0 +1,44 @@
+var APP_VERSION = '591eebf';
+var cacheName = 'personars-' + APP_VERSION;
+var filesToCache = [
+  './',
+  './index.html',
+  './personars.js',
+  './personars_bg.wasm',
+];
+
+/* Install: cache all files under the new version key */
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Activate: purge any caches from older versions */
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== cacheName) {
+            console.log('[SW] Removing old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+/* Cache-first: instant loads, cache invalidation handled by version bump */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
